29db37747041709e42cb37298d96f12acb5bc68f
abrarzahed <abrarzahed6986@gmail.com>
2023-06-20 16:35:59 +0600

    listed chapter 2 exercises

--- chapter2/ch2.js
+++ chapter2/ch2.js
@@ -364,3 +364,17 @@ thisWeek.add(52);
 thisWeek.add(49);
 console.log(thisWeek.average());
 */
+
+/****************************************** 
+COMMENT: Chapter 2 exercises   
+******************************************/
+/*
+1. Create a grades object that stores a set of student grades in an object. Provide a
+function for adding a grade and a function for displaying the student’s grade average.
+2. Store a set of words in an array and display the contents both forward and backward.
+3. Modify the weeklyTemps object in the chapter so that it stores a month’s worth of
+data using a two-dimensional array. Create functions to display the monthly aver‐
+age, a specific week’s average, and all the weeks’ averages.
+4. Create an object that stores individual letters in an array and has a function for
+displaying the letters as a single word.
+*/
